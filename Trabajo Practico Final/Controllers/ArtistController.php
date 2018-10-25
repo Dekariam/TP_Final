e36ed7d230db1be8626f0cb968e747228b1e88e1
Dekariam <43101910+Dekariam@users.noreply.github.com>
2018-10-25 15:41:09 -0300

    Update ArtistController.php

--- Trabajo Practico Final/Controllers/ArtistController.php
+++ Trabajo Practico Final/Controllers/ArtistController.php
@@ -2,27 +2,21 @@
 
 class ArtistController{
 
-	private $artistList;
+	public function__construct(){}
 
-	public function__construct(){
-		$this->artistList=   // levanta la lista de categorias de la base de datos
+	public function deleteArtist($artist){ // elimina un artista a la base de datos
+		// se llama al eliminar artista de dao para que lo elimine de la base de datos
+	}
+	public function updateArtist($artist){ // agregar un artista a la base de datos
+		// se llama al agregar artista de dao para que lo ingrese en la base de datos
 	}
 
-	public function addCategory ($artistName){
-		$newArtist = new Artist($artistName);
-		$this->artistList->add($newArtist);
+	public function returnArtist($nameArtist){ // buscar artista por su nombre
+		$artistList = //artistas de la base de datos (vienen de DAO)
+		//se busca ese artista por el nombre
+		$artist = // si lo encuentra devuelve el artista entero sino null
 	}
-
-	public function deleteArtist(){
-
-	}
-	public function updateArtist(){
-
-	}
-
-	public function returnArtist(){
-
-	}
+	
 }
 
 ?>
