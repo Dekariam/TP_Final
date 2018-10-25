1bc2f96b4ad901b39af18c6f9692f16b3f52f436
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-25 16:02:23 -0300

    Update CollectionDaos.php

--- Trabajo Practico Final/Interfaces/CollectionDaos.php
+++ Trabajo Practico Final/Interfaces/CollectionDaos.php
@@ -5,7 +5,7 @@
     public function return(); // devuelve la informacion de la base de datos
 	public function update(); // actualizar
 	public function delete(); // borrar una categoria
-	public function add(); // agrega una nueva categoria(perteneciente al evento) a la base de datos
+	public function add(); // agrega una nueva a la base de datos
    
    }
    
