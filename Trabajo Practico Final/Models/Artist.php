f90a6956e470b2358e9297f53749299612ff5b10
Dekariam <43101910+Dekariam@users.noreply.github.com>
2018-10-25 12:11:10 -0300

    Update Artist.php

--- Trabajo Practico Final/Models/Artist.php
+++ Trabajo Practico Final/Models/Artist.php
@@ -3,14 +3,15 @@
 class Artist{
 
 	private $artistName;
-
+	
+	/* Constructor del Artista */
 	public function__construct ($artistName){
 		$this->artistName = $artistName;
 	}
-
+	
+	/* Get del Artista */ 
 	public function getArtistName (){
 		return $this->artistName;
 	}
-
 }
  ?>
