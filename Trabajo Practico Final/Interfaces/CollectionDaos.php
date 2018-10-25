15aec0f0abee91bd97521b77cb777d5ae83313d2
Dekariam <43101910+Dekariam@users.noreply.github.com>
2018-10-25 16:20:24 -0300

    Update CollectionDaos.php

--- Trabajo Practico Final/Interfaces/CollectionDaos.php
+++ Trabajo Practico Final/Interfaces/CollectionDaos.php
@@ -2,11 +2,11 @@
 
   interface Collection {
     	
-	  public function modify($id,$object); // modifica la informacion de un objeto buscado por su id en la base de datos.
+	  public function modifyObject($id,$object); // modifica la informacion de un objeto buscado por su id en la base de datos.
 	
-	  public function delete($id); // borra un objeto por su id en la base de datos.
+	  public function deleteObject($id); // borra un objeto por su id en la base de datos.
 	
-	  public function update($object); // agrega un nuevo objeto a la base de datos.
+	  public function updateObject($object); // agrega un nuevo objeto a la base de datos.
 	  
 	  public function returnObject ($id); // retorna un objeto en caso de encontrarlo y en caso contrario retorna null.
 	  
