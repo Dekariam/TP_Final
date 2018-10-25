c048318d3e806cbf5a0072c91a31a2a02dcf5e15
juanikorn <44471843+juanikorn@users.noreply.github.com>
2018-10-25 12:47:36 -0300

    Update Router.php

--- Trabajo Practico Final/Config/Router.php
+++ Trabajo Practico Final/Config/Router.php
@@ -7,10 +7,19 @@
         /*redirreciona a la pagina de la peticion (request) */
         public static function Route(Request $request){
 
+            /*Guarda el nombre de la controladora que viene por request concatenando "Controller"*/
             $controllerName = $request->getcontroller() . 'Controller'; // Controller es por el nombre de la controladora EJ ArtistController
+           
+            /*Guarda el nombre del metodo que llega*/
             $methodName = $request->getmethod();
+           
+            /*Guarda los parametros del metodo*/
             $methodParameters = $request->getparameters();          
+            
+            /*el nombre de la clase usando "Controller\\" para acceder a la carpeta donde se encuentra*/  
             $controllerClassName = "Controllers\\". $controllerName;            
+            
+            /*instancia la controladora usando el nombre anterior*/
             $controller = new $controllerClassName;
             if(!isset($methodParameters))            
                 call_user_func(array($controller, $methodName));
