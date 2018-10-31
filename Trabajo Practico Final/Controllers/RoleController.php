e5e3cda55b4acbc48ba651d209ab93a4c14d30e3
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-31 13:52:00 -0300

    Update RoleController.php

--- Trabajo Practico Final/Controllers/RoleController.php
+++ Trabajo Practico Final/Controllers/RoleController.php
@@ -2,27 +2,30 @@
 
 class RoleController{
 	
-	private $roleList;
+	private $role;
 
 	public function__construct(){
-		$this->roleList= // levantar la lista de roles de la base de datos
+		// Tenemos que hacer la base de datos para levantar la instancia, y que hereden de singleton para poder usar el
+		// getInstance
+		//$this->role=RoleDao::getInstance();
 	}
 
 	public function addRole($idRole,$name){
 		$newRole = new Role($idRole,$name);
-		$this->roleList->add($newRole);
-	}
-
-	public function deleteRole (){
-
+		$this->role->add($newRole);
 	}
 	
-	public function updateRole(){
-
+	public function deleteRole($id){ // elimina de la base de datos.
+		// se llama al eliminar de dao para que lo elimine de la base de datos.
 	}
-
-	public function returnRole(){
-		
+	
+	public function modifyRole($idRole){
+		// se llama a la funcion de dao que modifica por su ID.
+	}
+	public function returnRole($idRole){ // buscar por ID.
+		$roleList = //de la base de datos (vienen de DAO).
+		//se busca ese objeto por el nombre.
+		$role = // si lo encuentra devuelve el objeto entero sino null.
 	}
 
 } 
