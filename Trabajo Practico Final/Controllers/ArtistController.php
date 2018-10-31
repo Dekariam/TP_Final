e829bc535889f091f694bf194ff7c57e167643b2
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-31 13:25:07 -0300

    Update ArtistController.php

--- Trabajo Practico Final/Controllers/ArtistController.php
+++ Trabajo Practico Final/Controllers/ArtistController.php
@@ -2,14 +2,19 @@
 
 class ArtistController{
 
-	public function__construct(){}
+	private $artist;
+	public function__construct(){
+		// Tenemos que hacer la base de datos para levantar la instancia, y que hereden de singleton para poder usar el
+		// getInstance
+		//$this->artist=ArtistDao::getInstance();
+	}
 
 	// en caso de llamar a la interfaz usar esos metodos MODIFICAR.
 	
 	public function deleteArtist($id){ // elimina un artista a la base de datos.
 		// se llama al eliminar artista de dao para que lo elimine de la base de datos.
 	}
-	public function updateArtist($artist){ // agregar un artista a la base de datos.
+	public function addArtist($artist){ // agregar un artista a la base de datos.
 		// se llama al agregar artista de dao para que lo ingrese en la base de datos.
 	}
 	
