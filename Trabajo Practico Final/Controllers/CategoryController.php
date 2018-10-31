f939932fa7c74638ca2c266ca648596b90daffd5
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-31 13:37:50 -0300

    Update CategoryController.php

--- Trabajo Practico Final/Controllers/CategoryController.php
+++ Trabajo Practico Final/Controllers/CategoryController.php
@@ -2,29 +2,33 @@
 
 class CategoryController{
 
-	private $categoryList;
+	private $category;
 
 	public function__construct(){
-		$this->categoryList=   // levanta la lista de categorias de la base de datos
+		// Tenemos que hacer la base de datos para levantar la instancia, y que hereden de singleton para poder usar el
+		// getInstance
+		//$this->category=CategoryDao::getInstance();
 	}
 
 	// agregar una nueva categoria 
 	public function addCategory ($idDescription,$description){
 		$newCategory = new Category($idDescription,$description);
-		$this->categoryList->add($newCategory);
+		$this->category->add($newCategory);
 	}
 
-	public function deleteCategory(){
-
+	public function deleteCategory($id){ // elimina de la base de datos.
+		// se llama al eliminar de dao para que lo elimine de la base de datos.
 	}
 	
-	public function updateCategory(){
-
+	public function modifyCategory($idCategory){
+		// se llama a la funcion de dao que modifica por su ID.
 	}
-
-	public function returnCategory(){
-
+	public function returnCategory($idCategory){ // buscar por ID.
+		$categoryList = //de la base de datos (vienen de DAO).
+		//se busca ese objeto por el nombre.
+		$category = // si lo encuentra devuelve el objeto entero sino null.
 	}
+	
 }
 
 ?>
