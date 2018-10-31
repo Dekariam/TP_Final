70819883bb427f046bf0f1f91eebdf9bdcbd3d9f
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-31 13:39:22 -0300

    Update EventController.php

--- Trabajo Practico Final/Controllers/EventController.php
+++ Trabajo Practico Final/Controllers/EventController.php
@@ -1,6 +1,33 @@
 <?php namespace Controllers;
+//llamar a todos los use, e implementar interfaz probablemente
 
 class EventController{
+	
+	private $event
+		
+	public function__construct(){
+		// Tenemos que hacer la base de datos para levantar la instancia, y que hereden de singleton para poder usar el
+		// getInstance
+		//$this->event=EventDao::getInstance();
+	}
+	// en caso de llamar a la interfaz usar esos metodos MODIFICAR.
+	
+	public function deleteEvent($id){ // elimina de la base de datos.
+		// se llama al eliminar de dao para que lo elimine de la base de datos.
+	}
+	public function addEvent($calendar){ // agregar a la base de datos.
+		// se llama al agregar de dao para que lo ingrese en la base de datos.
+	}
+	
+	public function modifyEvent($idCalendar){
+		// se llama a la funcion de dao que modifica por su ID.
+	}
+	public function returnEvent($idCalendar){ // buscar por ID.
+		$eventList = //de la base de datos (vienen de DAO).
+		//se busca ese objeto por el nombre.
+		$event = // si lo encuentra devuelve el objeto entero sino null.
+	}
+	
 	
 } 
 
