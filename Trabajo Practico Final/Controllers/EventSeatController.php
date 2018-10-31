08289f5e4fdda58b6e4041269975f51e81ddefe1
Shamanvoljin <44471843+Shamanvoljin@users.noreply.github.com>
2018-10-31 13:43:52 -0300

    Update EventSeatController.php

--- Trabajo Practico Final/Controllers/EventSeatController.php
+++ Trabajo Practico Final/Controllers/EventSeatController.php
@@ -2,6 +2,31 @@
 
 class EventSeatController{
 	
+	private $eventSeat
+		
+	public function__construct(){
+		// Tenemos que hacer la base de datos para levantar la instancia, y que hereden de singleton para poder usar el
+		// getInstance
+		//$this->eventSeat=EventSeatDao::getInstance();
+	}
+	// en caso de llamar a la interfaz usar esos metodos MODIFICAR.
+	
+	public function deleteEventSeat($id){ // elimina de la base de datos.
+		// se llama al eliminar de dao para que lo elimine de la base de datos.
+	}
+	public function addEventSeat($EventSeat){ // agregar a la base de datos.
+		// se llama al agregar de dao para que lo ingrese en la base de datos.
+	}
+	
+	public function modifyEventSeat($idEventSeat){
+		// se llama a la funcion de dao que modifica por su ID.
+	}
+	public function returnEventSeat($idEventSeat){ // buscar por ID.
+		$eventPlaceList = //de la base de datos (vienen de DAO).
+		//se busca ese objeto por el nombre.
+		$eventPlace = // si lo encuentra devuelve el objeto entero sino null.
+	}
+	
 } 
 
  ?>
